14a29daf978154f699f3ae5e2c49798dafacf1a7
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-03-16 19:14:16 -0500

    Cambios implementados en cout, cin y endl

--- POSGSOFT/menu.cpp
+++ POSGSOFT/menu.cpp
@@ -2,14 +2,16 @@
 
 int menu(){
    int opcion;
-   std::cout << "\n!---Menu actas---!\n"<< std::endl;
-   std::cout << "1. Agregar acta" << std::endl;
-   std::cout << "2. Mostrar actas" << std::endl;
-   std::cout << "3. Ingreso jurado" << std::endl;
-   std::cout << "4. Crear criterios de calificacion" << std::endl;
-   std::cout << "0. Salir\n" << std::endl;
-   std::cout << "Digita el numero: ";
-   std::cin >> opcion;
-   std::cout << std::endl;
+   
+   cout << "\n!---Menu actas---!\n"<< endl;
+   cout << "1. Agregar acta" << endl;
+   cout << "2. Mostrar actas" << endl;
+   cout << "3. Ingreso jurado" << endl;
+   cout << "4. Crear criterios de calificacion" << endl;
+   cout << "0. Salir\n" << endl;
+   cout << "Digita el numero: ";
+   cin >> opcion;
+   cout << endl;
+   
    return opcion;
 }
