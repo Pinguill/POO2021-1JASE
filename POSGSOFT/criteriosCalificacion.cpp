4c4aaa127f780bc7a313735c8c76512f06aa3de0
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-03-16 19:06:28 -0500

    Métodos implementados

--- POSGSOFT/criteriosCalificacion.cpp
+++ POSGSOFT/criteriosCalificacion.cpp
@@ -3,10 +3,52 @@
 Criterio::Criterio(){
 }
 
-Criterio::Criterio( string identificador, string textoPresentado, float ponderado, string mensajeJurado, float notaCriterio  ){
+Criterio::Criterio( string identificador, string textoPresentado, float ponderado, string mensajeJurado, float notaCriterio  )
+{
    this -> identificador = identificador;
    this -> textoPresentado = textoPresentado;
    this -> ponderado = ponderado;
    this -> mensajeJurado = mensajeJurado;
    this -> notaCriterio = notaCriterio;
+}
+
+
+void Criterio::llenarCriterio()
+{
+   cin.ignore( 30, '\n' );
+   cout << "\nTítulo de criterio: "; getline( cin, identificador );
+
+   // Este está en duda, no sé bien si sea descripción
+   cin.ignore( 30, '\n' );
+   cout << "\n\nDescripción del criterio: "; getline( cin, textoPresentado );
+
+   cout << "\nPonderación: "; 
+   cin >> ponderado;
+
+}
+
+void Criterio::calificarCriterio()
+{
+   cout << "Nota pertinente: "; cin >> notaCriterio;
+}
+
+void Criterio::escribirMensaje()
+{
+   cin.ignore( 40, '\n' );
+   cout << "Digite la observacion: "; getline( cin, mensajeJurado ); 
+}
+
+void Criterio::imprimirMensaje()
+{
+   cout << mensajeJurado;
+}
+
+// Este puede estar incluído en el main
+void Criterio::consultarListaCriterios()
+{
+   int i;
+   for( i = 0; i < listaCriterios.size(); i++ )
+   {
+      cout << listaCriterios[i].identificador << std::endl;
+   }
 }
