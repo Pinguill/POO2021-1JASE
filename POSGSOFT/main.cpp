6509a98f7b3151927729bdbebff3c69d7e426b35
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-03-17 12:51:55 -0500

    Nuevos métodos y experto.h implementado

--- POSGSOFT/main.cpp
+++ POSGSOFT/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "menu.h"
+#include "experto.h"
 #include "actaTrabajo.h"
 #include "criteriosCalificacion.h"
 
 int main(){
    system( "CLS()" );
    ActaTrabajo acta;
-   int opc;
+   Experto jurado;
+   int opc, opc2;
+
    do{
       opc = menu();
       switch ( opc ){
@@ -17,10 +20,32 @@ int main(){
          acta.mostrarActa();
          break;
       case 3:
+         jurado.registrarExperto();
          break;
       case 4:
          break;
       case 5:
+         do{
+            opc2 = menu2();
+
+            switch( opc2 )
+            {
+            case 1:
+               acta.consultarTipoTrabajo();
+               break;
+            case 6:
+               acta.consultarEstadoAceptacion();
+               break;
+            case 7:
+               jurado.consultarJurados();
+               break;
+            case 0:
+               break;
+            default:
+               cout << "\nNo disponible" << endl;
+               break;
+            }
+         } while( opc2 != 0 );
          break;
       default:
          break;
