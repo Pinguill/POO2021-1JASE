70d04e9c37f76cee8bba20a9d48e0f6a95e03ea2
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-03-17 12:53:53 -0500

    Nuevo menú añadido y correcciones

--- POSGSOFT/menu.cpp
+++ POSGSOFT/menu.cpp
@@ -2,16 +2,34 @@
 
 int menu(){
    int opcion;
-   
+
+   system("CLS()");
    cout << "\n!---Menu actas---!\n"<< endl;
    cout << "1. Agregar acta" << endl;
    cout << "2. Mostrar actas" << endl;
    cout << "3. Ingreso jurado" << endl;
    cout << "4. Crear criterios de calificacion" << endl;
+   cout << "5. Informacion adicional" << endl;
    cout << "0. Salir\n" << endl;
    cout << "Digita el numero: ";
    cin >> opcion;
    cout << endl;
    
    return opcion;
+}
+
+int menu2()
+{
+   int opcion2;
+
+   system("CLS()");
+   cout << "\n!---Menu Informacion---!\n" << endl;
+   cout << "1. Actas Aplicadas y Investigativas desarrolladas" << endl;
+   cout << "6. Actas Pendientes y Rechazadas " << endl;
+   cout << "7. Jurados externos e internos" << endl;
+   cout << "0. Salir\n" << endl;
+   cout << "Digita el numero: ";
+   cin >> opcion2;
+
+   return opcion2;
 }
