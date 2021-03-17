50a793f7cd72caaa017dd969f9ab91f7a02d44f9
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-03-16 19:10:55 -0500

    Vector y métodos añadidos

--- POSGSOFT/experto.h
+++ POSGSOFT/experto.h
@@ -7,18 +7,38 @@
 
 using std::vector;
 using std::string;
+using std::cout;
+using std::cin;
 
 vector <string> nombre;
 
+enum ingreso   // Verifica si el experto se ha registrado
+{
+   REGISTRADO,
+   NO_REGISTRADO
+};
+
+enum sectorExperto   // Indica si el jurado es profesor de universidad o no
+{
+   INTERNO,
+   EXTERNO
+};
+
 class Experto{
    private:
-      bool sector;
+      string user;
+      string password;
+      sectorExperto sector;   // Se refiere a si el jurado es interno o externo, lo cambié de Bool a Enum
       string nombre;
       int trabajosDirigidos;
+      ingreso registro;
+      vector <Experto> expertosCreados;   // Añadí este vector 
 
    public:
       Experto();
-      Experto( bool, string, int );
+      Experto( string, string, sectorExperto, string );
+      void registrarExperto();
+      void consultarJurados(); // Consulta jurados externos e internos
       void getExperto();
 };
 
