7d4a2ee1054bf441d6d6b2120a43eb3ae9c1d0bc
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-03-19 20:26:45 -0500

    Indicaciones de validación añadidos

--- POSGSOFT/conjuntoLista.h
+++ POSGSOFT/conjuntoLista.h
@@ -4,6 +4,8 @@
 #include "actaTrabajo.h"
 #include "criteriosCalificacion.h"
 #include <vector>
+#include <cctype>
+ #include <algorithm>
 
 using std::vector;
 using std::string;
@@ -11,23 +13,25 @@ using std::cout;
 using std::cin;
 
 class Universidad{
-private:
-   int contadorTrabajoAplicado = 0; 
-   int contadorTrabajoInvestigacion= 0;
-   vector <Experto> vectorPersonas;
-   vector <ActaTrabajo> vectorActasTrabajos;
-   vector <Criterio> vectorCriterios;
-public:
-   Universidad();
-   void mostrarActas();
-   void mostrarPersona();
-   void mostrarCriterio();
-   void crearActa();
-   void consultarTipoTrabajo();
-   void consultarEstadoAceptacion();
-   void registrarExperto();
-   void consultarJurados();
-   void cerrarActa();
+   private:
+      int contadorTrabajoAplicado = 0; 
+      int contadorTrabajoInvestigacion= 0;
+      vector <Experto> vectorPersonas;
+      vector <ActaTrabajo> vectorActasTrabajos;
+      vector <Criterio> vectorCriterios;
+
+   public:
+      Universidad();
+      void mostrarActas(); // No tiene validaciones
+      void mostrarPersona();  // No tiene validaciones
+      void mostrarCriterio(); // No tiene validaciones
+      void crearActa(); // Falta validar cuando el usuario pone un valor string en vez de int en menús
+      void consultarTipoTrabajo();  //Validaciones hechas
+      void consultarEstadoAceptacion();   // Validaciones hechas
+      void registrarExperto();   //  Falta validar cuando el usuario pone un valor string en vez de int en menús
+      void consultarJurados();   // Validaciones hechas
+      void cerrarActa();   // Validaciones hechas
+      void consultarTrabajoProfesor(); //  Falta validar cuando el usuario pone un valor string en vez de int en menús
 };
 
 #endif
